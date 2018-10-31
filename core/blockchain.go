ab13d1b58c5f1de844d97bf877199d71a72b8ae7
najimmy <najimmy@gmail.com>
2018-10-31 11:20:10 +0900

    add default func

--- core/blockchain.go
+++ core/blockchain.go
@@ -1,1 +1,35 @@
 package core
+
+import (
+	// "github.com/ethereum/go-ethereum/core/types"
+
+	"github.com/najimmy/go-simplechain/common"
+	"github.com/najimmy/go-simplechain/core"
+	"github.com/najimmy/go-simplechain/storage"
+)
+
+/*
+First time
+search the height or the hash
+add block
+ignore block validity
+*/
+type BlockChain struct {
+	storage storage.Storage
+}
+
+func NewBlockChain() (*BlockChain, error) {
+	return nil, nil
+}
+
+func (bc *BlockChain) GetBlockByHash(hash common.Hash) *core.Block {
+	return nil
+}
+
+func (bc *BlockChain) GetBlockByHeight(height uint64) *core.Block {
+	return nil
+}
+
+func (bc *BlockChain) PutBlock(block *core.Block) {
+	return nil
+}
