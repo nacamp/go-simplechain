9b2f1baff5dd8984ad251148a1aee7dd41b9dd21
najimmy <najimmy@gmail.com>
2018-11-01 15:05:50 +0900

    change Height data type, add Hash func

--- core/block.go
+++ core/block.go
@@ -3,6 +3,8 @@ package core
 import (
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/najimmy/go-simplechain/common"
 )
 
@@ -10,11 +12,29 @@ import (
 type Header struct {
 	ParentHash common.Hash
 	Coinbase   common.Address
-	Number     *big.Int
+	Height     uint64
 	Time       *big.Int
+	Hash       common.Hash
 }
 
 // Simple Block
 type Block struct {
 	Header *Header
+	//next
+	//transactions Transactions
+}
+
+func (b *Block) Hash() common.Hash {
+	return b.Header.Hash
+}
+
+func (b *Block) MakeHash() {
+	hasher := sha3.New256()
+	rlp.Encode(hasher, []interface{}{
+		b.Header.ParentHash,
+		b.Header.Coinbase,
+		b.Header.Height,
+		b.Header.Time,
+	})
+	hasher.Sum(b.Header.Hash[:0])
 }
