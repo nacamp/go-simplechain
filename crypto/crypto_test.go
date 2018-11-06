26548b37b551699ca858bdf23e20557f00c1058c
najimmy <najimmy@gmail.com>
2018-11-06 10:14:42 +0900

    generate test address

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -24,11 +24,12 @@ func TestAddress(t *testing.T) {
 		priv/pub
 		0xe68fb0a479c495910c8351c3593667028b45d679f55ce22b0514c4a8a6bcbdd1 / 0x036407c079c962872d0ddadc121affba13090d99a9739e0d602ccfda2dab5b63c0
 		0xf390e256b6ed8a1b283d3ea80b103b868c14c31e5b7114fc32fff21c4cb263eb / 0x03fdefdefbb2478f3d1ed3221d38b8bad6d939e50f17ffda40f0510b4d28506bd3
+		0xb385aca81e134722cca902bf85443528c3d3a783cf54008cfc34a2ca563fc5b6 / 0x03e864b08b08f632c61c6727cde0e23d125f7784b5a5a188446fc5c91ffa51faa1
 	*/
 	pubkey := priv.PubKey()
 	address := common.BytesToAddress(pubkey.SerializeCompressed())
-	assert.Equal(t, pubkey.SerializeCompressed(), address[:], "")
-	//fmt.Println(common.ToHex(priv.Serialize()))
-	//fmt.Println(common.ToHex(pubkey.SerializeCompressed()))
+	// fmt.Println(common.ToHex(priv.Serialize()))
+	// fmt.Println(common.ToHex(pubkey.SerializeCompressed()))
 	//fmt.Println(common.ToHex(pubkey.SerializeUncompressed()))
+	assert.Equal(t, pubkey.SerializeCompressed(), address[:], "")
 }
