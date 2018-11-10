808d3d0b1e2c1d5d4edbde9a892438bdfdeb54f0
najimmy <najimmy@gmail.com>
2018-11-10 21:05:08 +0900

    change var name

--- core/block.go
+++ core/block.go
@@ -29,7 +29,7 @@ type Block struct {
 
 	AccountState     *AccountState
 	TransactionState *TransactionState
-	MinderState      MinerState
+	MinerState       MinerState
 	VoterState       *AccountState
 }
 
