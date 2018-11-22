042d1d05a9607e6597cba07f9b468d09ccda14e5
najimmy <najimmy@gmail.com>
2018-11-23 01:04:10 +0900

    add MakeConfig fix set time for coinbase mining at MakeBlock

--- tests/fixture.go
+++ tests/fixture.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"crypto/ecdsa"
+	"encoding/json"
 	"fmt"
 	"math/big"
 
@@ -28,11 +29,48 @@ const (
 	LangObjC
 )
 
+func MakeConfig() *core.Config {
+	configStr := `{
+		"host_id" : "/ip4/127.0.0.1/tcp/9990/ipfs/16Uiu2HAkwR1pV8ZR8ApcZWrMSw5iNMwaJHFpKr91H9a1a65WGehk",
+		"db_path" : "/opt/simplechain/data",
+		"miner_address" : "0x036407c079c962872d0ddadc121affba13090d99a9739e0d602ccfda2dab5b63c0",
+		"miner_private_key" : "0xe68fb0a479c495910c8351c3593667028b45d679f55ce22b0514c4a8a6bcbdd1",
+		"seeds" :  ["080212201afa45f64cd5a28cd40e178889ed2e9f987658bc4d48d376ef6ecb1ab1b26211"],
+		"voters" : [{"address":"0x036407c079c962872d0ddadc121affba13090d99a9739e0d602ccfda2dab5b63c0", "balance":100 },
+				    {"address":"0x03fdefdefbb2478f3d1ed3221d38b8bad6d939e50f17ffda40f0510b4d28506bd3", "balance":20 },
+					{"address":"0x03e864b08b08f632c61c6727cde0e23d125f7784b5a5a188446fc5c91ffa51faa1", "balance":50 }]
+		}`
+	contents := []byte(configStr)
+	config := &core.Config{}
+	json.Unmarshal([]byte(contents), config)
+	return config
+}
+func MakeVoterAccountsFromConfig(config *core.Config) (voters []*core.Account) {
+	voters = make([]*core.Account, 3)
+	for i, voter := range config.Voters {
+		account := &core.Account{}
+		copy(account.Address[:], common.FromHex(voter.Address))
+		account.Balance = voter.Balance
+		voters[i] = account
+	}
+	return voters
+}
+
 func MakeBlock(bc *core.BlockChain, parentBlock *core.Block, coinbase, from, to string, amount *big.Int, trickId trick, trickValue interface{}) *core.Block {
 	h := &core.Header{}
 	h.ParentHash = parentBlock.Hash()
 	h.Height = parentBlock.Header.Height + 1
 	h.Time = parentBlock.Header.Time + 3
+	//set time to turn coinbase mining
+	gMs, _ := bc.GenesisBlock.MinerState.Clone()
+	gMinerGroup, _ := gMs.MakeMiner(bc.GenesisBlock.VoterState, 3)
+	for {
+		index := (h.Time % 9) / 3
+		if gMinerGroup[index] == common.HexToAddress(coinbase) {
+			break
+		}
+		h.Time++
+	}
 	block := &core.Block{Header: h}
 
 	//voter
