8b3733a2f2518b5b843df391bf8d7b0aef2348cf
najimmy <najimmy@gmail.com>
2018-11-27 19:03:18 +0900

    use queue and map together

--- core/transaction_pool.go
+++ core/transaction_pool.go
@@ -1,31 +1,50 @@
 package core
 
+import "github.com/najimmy/go-simplechain/common"
+
 type TransactionPool struct {
-	queue []*Transaction
+	queue []common.Hash
+	txMap map[common.Hash]*Transaction
 }
 
 func NewTransactionPool() *TransactionPool {
-	return &TransactionPool{}
+	return &TransactionPool{txMap: make(map[common.Hash]*Transaction)}
 }
 
 func (pool *TransactionPool) Put(tx *Transaction) {
-	pool.queue = append(pool.queue, tx)
+	pool.txMap[tx.Hash] = tx
+	pool.queue = append(pool.queue, tx.Hash)
 }
 
 func (pool *TransactionPool) Pop() (tx *Transaction) {
-	if len(pool.queue) > 0 {
-		tx = pool.queue[0]
-		pool.queue = pool.queue[1:]
-		return tx
+	for {
+		if len(pool.queue) > 0 {
+			hash := pool.queue[0]
+			pool.queue = pool.queue[1:]
+			tx = pool.txMap[hash]
+			if tx != nil {
+				return tx
+			}
+		} else {
+			break
+		}
 	}
+
 	return nil
 }
 
 func (pool *TransactionPool) Peek() (tx *Transaction) {
 	if len(pool.queue) > 0 {
-		tx = pool.queue[0]
-		pool.queue = pool.queue[1:]
-		return tx
+		hash := pool.queue[0]
+		return pool.txMap[hash]
 	}
 	return nil
+}
+func (pool *TransactionPool) Get(hash common.Hash) (tx *Transaction) {
+	return pool.txMap[hash]
+}
+
+//TODO: remove hash in queue
+func (pool *TransactionPool) Del(hash common.Hash) {
+	delete(pool.txMap, hash)
 }
