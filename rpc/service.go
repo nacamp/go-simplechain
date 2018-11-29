99568b2617663edf41cbb4442d1f8713647b88c6
najimmy <najimmy@gmail.com>
2018-11-29 10:06:07 +0900

    add getBalance rpc call

--- rpc/service.go
+++ rpc/service.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/intel-go/fastjson"
 
+	"github.com/najimmy/go-simplechain/common"
 	"github.com/najimmy/go-simplechain/core"
 	"github.com/osamingo/jsonrpc"
 )
@@ -34,22 +35,64 @@ func (h *AccountsHandler) Result() interface{} {
 
 // accounts <<<<<<<<<<
 
+// getBalance >>>>>>>>>>>
+type GetBalanceHandler struct {
+	bc *core.BlockChain
+}
+
+func NewGetBalanceHandler(bc *core.BlockChain) *GetBalanceHandler {
+
+	return &GetBalanceHandler{bc: bc}
+}
+
+func (h *GetBalanceHandler) Name() string {
+	return "getBalance"
+}
+
+func (h *GetBalanceHandler) ServeJSONRPC(c context.Context, params *fastjson.RawMessage) (interface{}, *jsonrpc.Error) {
+	p := []string{}
+	if err := jsonrpc.Unmarshal(params, &p); err != nil {
+		return nil, err
+	}
+	account := h.bc.Tail.AccountState.GetAccount(common.HexToAddress(p[0]))
+	return account.Balance.String(), nil
+}
+
+func (h *GetBalanceHandler) Params() interface{} {
+	return []string{}
+}
+func (h *GetBalanceHandler) Result() interface{} {
+	return ""
+}
+
+// getBalance <<<<<<<<<<
+
 type RpcService struct {
 	server *RpcServer
 }
 
-func (rs *RpcService) Setup(server *RpcServer, config *core.Config) {
+func (rs *RpcService) Setup(server *RpcServer, config *core.Config, bc *core.BlockChain) {
 	rs.server = server
 	rs.server.RegisterHandler(NewAccountsHandler(config))
+	rs.server.RegisterHandler(NewGetBalanceHandler(bc))
 }
 
 /*
-curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0",   "method": "accounts", "params":[]}' http://localhost:8080/jrpc
 https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_accounts
+
+curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0",   "method": "accounts", "params":[]}' http://localhost:8080/jrpc
 curl -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[],"id":1}'
 {
   "id":1,
   "jsonrpc": "2.0",
   "result": ["0xc94770007dda54cF92009BFF0dE90c06F603a09f"]
+}
+
+curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc": "2.0",   "method": "getBalance", "params":["0x036407c079c962872d0ddadc121affba13090d99a9739e0d602ccfda2dab5b63c0"]}' http://localhost:8080/jrpc
+curl -X POST --data '{"jsonrpc":"2.0","method":"eth_getBalance","params":["0xc94770007dda54cF92009BFF0dE90c06F603a09f", "latest"],"id":1}'
+{
+  "id":1,
+  "jsonrpc": "2.0",
+  "result": "0x0234c8a3397aab58" // 158972490234375000
 }
 */
