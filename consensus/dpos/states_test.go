d906e64eb50441aa78e7366218a94272b82590fe
nacamp <najimmy@gmail.com>
2019-03-03 13:32:25 +0900

    test

--- consensus/dpos/states_test.go
+++ consensus/dpos/states_test.go
@@ -3,6 +3,7 @@ package dpos
 import (
 	"bytes"
 	"math/big"
+	"reflect"
 	"testing"
 
 	"github.com/nacamp/go-simplechain/rlp"
@@ -41,219 +42,56 @@ func TestStakeUnstake(t *testing.T) {
 	assert.Error(t, err)
 }
 
+func TestStake(t *testing.T) {
+	_storage, _ := storage.NewMemoryStorage()
+	state, err := NewInitState(common.Hash{}, 0, _storage)
+	assert.NoError(t, err)
+	_ = state
+
+	var newAddr = "0x1df75c884f7f1d1537177a3a35e783236739a426ee649fa3e2d8aed598b4f29e838170e2"
+	state.Stake(tests.Address0, common.HexToAddress(newAddr), new(big.Int).SetUint64(10))
+	_, err = state.GetNewRoundMiners(uint64(5), 3)
+	assert.Error(t, err)
+	state.Stake(tests.Address0, tests.Address0, new(big.Int).SetUint64(30))
+	state.Stake(tests.Address0, tests.Address1, new(big.Int).SetUint64(40))
+	state.Stake(tests.Address0, tests.Address2, new(big.Int).SetUint64(50))
+
+	//test GetNewRoundMiners
+	miners, _ := state.GetNewRoundMiners(uint64(5), 3)
+	minerSize := 0
+	for _, v := range miners {
+		if v == tests.Address0 || v == tests.Address1 || v == tests.Address2 {
+			minerSize++
+		}
+	}
+	assert.Equal(t, 3, len(miners))
+	assert.Equal(t, 3, minerSize)
+
+	//test PutMiners and GetMiners
+	minersHash, _ := state.PutMiners(miners)
+	miners2, _ := state.GetMiners(minersHash)
+	assert.True(t, reflect.DeepEqual(miners, miners2))
+
+	//test Put and Get
+	state.Put(1, 5, minersHash)
+	candidateHash, votersHash, minersHash, electedTime, err := state.Get(1)
+	assert.Equal(t, state.Candidate.RootHash(), common.HashToBytes(candidateHash))
+	assert.Equal(t, state.Voter.RootHash(), common.HashToBytes(votersHash))
+	assert.Equal(t, minersHash, minersHash)
+	assert.Equal(t, electedTime, uint64(5))
+
+	//test NewInitState
+	state2, err := NewInitState(state.RootHash(), 1, _storage)
+	assert.Equal(t, state.Candidate.RootHash(), state2.Candidate.RootHash())
+	assert.Equal(t, state.Voter.RootHash(), state2.Voter.RootHash())
+	assert.Equal(t, minersHash, state2.MinersHash)
+	assert.Equal(t, uint64(5), state2.ElectedTime)
+
+	//TODO test
+	//ExecuteTransaction in blockchain
+}
+
 func TestGetNewElectedTime(t *testing.T) {
 	assert.Equal(t, uint64(0), GetNewElectedTime(0, 26, 3, 3, 3))
 	assert.Equal(t, uint64(27), GetNewElectedTime(0, 27, 3, 3, 3))
 }
-
-/*
-func (ds *DposState) GetMiners(minerHash common.Hash) ([]common.Address, error) {
-	miner := []common.Address{}
-	decodedBytes, _ := ds.Miner.Get(minerHash[:])
-	rlp.NewStream(bytes.NewReader(decodedBytes), 0).Decode(&miner)
-	return miner, nil
-}
-
-func (ds *DposState) GetNewRoundMiners(electedTime uint64, totalMiners int) ([]common.Address, error) {
-	iter, err := ds.Candidate.Iterator(nil)
-	if err != nil {
-		return nil, err
-	}
-	exist, _ := iter.Next()
-	candidates := []core.BasicAccount{}
-	for exist {
-		account := core.BasicAccount{Address: common.Address{}}
-
-		// encodedBytes1 := iter.Key()
-		// key := new([]byte)
-		// rlp.NewStream(bytes.NewReader(encodedBytes1), 0).Decode(key)
-		account.Address = common.BytesToAddress(iter.Key())
-
-		encodedBytes2 := iter.Value()
-		value := new(big.Int)
-		rlp.NewStream(bytes.NewReader(encodedBytes2), 0).Decode(value)
-		account.Balance = value
-
-		candidates = append(candidates, account)
-		exist, err = iter.Next()
-	}
-
-	if len(candidates) < totalMiners {
-		return nil, errors.New("The number of candidated miner is smaller than the minimum miner number.")
-	}
-
-	sort.Slice(candidates, func(i, j int) bool {
-		return candidates[i].Balance.Cmp(candidates[j].Balance) > 0
-	})
-
-	candidates = candidates[:totalMiners]
-	candidateAddrs := []common.Address{}
-	for _, v := range candidates {
-		candidateAddrs = append(candidateAddrs, v.Address)
-	}
-	shuffle(candidateAddrs, int64(electedTime))
-	return candidateAddrs, nil
-}
-
-func (ds *DposState) PutMiners(miners []common.Address) (hash common.Hash, err error) {
-	encodedBytes, err := rlp.EncodeToBytes(miners)
-	if err != nil {
-		return common.Hash{}, err
-	}
-	hashBytes := crypto.Sha3b256(encodedBytes)
-	ds.Miner.Put(hashBytes, encodedBytes)
-	copy(hash[:], hashBytes)
-	return hash, nil
-}
-
-func (ds *DposState) Put(blockNumber, electedTime uint64, minersHash common.Hash) error {
-	vals := make([]byte, 0)
-	keyEncodedBytes, err := rlp.EncodeToBytes(blockNumber)
-	if err != nil {
-		return err
-	}
-	encodedTimeBytes, err := rlp.EncodeToBytes(electedTime)
-	if err != nil {
-		return err
-	}
-
-	vals = append(vals, ds.Candidate.RootHash()...)
-	vals = append(vals, ds.Voter.RootHash()...)
-	vals = append(vals, minersHash[:]...)
-	vals = append(vals, encodedTimeBytes...)
-	_, err = ds.Miner.Put(crypto.Sha3b256(keyEncodedBytes), vals)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-
-func (ds *DposState) Get(blockNumber uint64) (common.Hash, common.Hash, common.Hash, uint64, error) {
-	keyEncodedBytes, err := rlp.EncodeToBytes(blockNumber)
-	if err != nil {
-		return common.Hash{}, common.Hash{}, common.Hash{}, 0, err
-	}
-	//TODO: check minimum key size
-	encbytes, err := ds.Miner.Get(crypto.Sha3b256(keyEncodedBytes))
-	if err != nil {
-		return common.Hash{}, common.Hash{}, common.Hash{}, 0, err
-	}
-	if len(encbytes) < common.HashLength*3 {
-		return common.Hash{}, common.Hash{}, common.Hash{}, 0, errors.New("Bytes lenght must be more than 64 bits")
-	}
-
-	electedTime := uint64(0)
-	err = rlp.Decode(bytes.NewReader(encbytes[common.HashLength*3:]), &electedTime)
-	if err != nil {
-		return common.Hash{}, common.Hash{}, common.Hash{}, 0, err
-	}
-	return common.BytesToHash(encbytes[:common.HashLength]),
-		common.BytesToHash(encbytes[common.HashLength : common.HashLength*2]),
-		common.BytesToHash(encbytes[common.HashLength*2 : common.HashLength*3]),
-		electedTime, nil
-}
-
-func (ds *DposState) RootHash() (hash common.Hash) {
-	copy(hash[:], ds.Miner.RootHash())
-	return hash
-}
-
-func (ds *DposState) Clone() (core.ConsensusState, error) {
-	tr1, err1 := ds.Candidate.Clone()
-	if err1 != nil {
-		return nil, err1
-	}
-	tr2, err2 := ds.Miner.Clone()
-	if err2 != nil {
-		return nil, err2
-	}
-	tr3, err3 := ds.Voter.Clone()
-	if err3 != nil {
-		return nil, err3
-	}
-	return &DposState{
-		Candidate:   tr1,
-		Miner:       tr2,
-		Voter:       tr3,
-		MinersHash:  ds.MinersHash,
-		ElectedTime: ds.ElectedTime,
-	}, nil
-}
-
-func (cs *DposState) ExecuteTransaction(block *core.Block, txIndex int, account *core.Account) (err error) {
-
-	tx := block.Transactions[txIndex]
-	amount := new(big.Int)
-	err = rlp.Decode(bytes.NewReader(tx.Payload.Data), amount)
-	if err != nil {
-		return err
-	}
-	if tx.Payload.Code == core.TxCVoteStake {
-		err = account.Stake(tx.To, amount)
-		if err != nil {
-			return err
-		}
-		return cs.Stake(account.Address, tx.To, amount)
-	} else if tx.Payload.Code == core.TxCVoteUnStake {
-		err = account.UnStake(tx.To, amount)
-		if err != nil {
-			return err
-		}
-		return cs.Unstake(account.Address, tx.To, amount)
-	}
-	return nil
-}
-
-
-
-
-
-func shuffle(slice []common.Address, seed int64) {
-	r := rand.New(rand.NewSource(seed))
-	for len(slice) > 0 {
-		n := len(slice)
-		randIndex := r.Intn(n)
-		slice[n-1], slice[randIndex] = slice[randIndex], slice[n-1]
-		slice = slice[:n-1]
-	}
-}
-
-func (bc *BlockChain) PutMinerState(block *Block) error {
-
-	// save status
-	ms := block.MinerState
-	minerGroup, voterBlock, err := ms.GetMinerGroup(bc, block)
-	if err != nil {
-		return err
-	}
-	//TODO: we need to test  when voter transaction make
-	//make new miner group
-	if voterBlock.Header.Height == block.Header.Height {
-
-		ms.Put(minerGroup, block.Header.VoterHash) //TODO voterhash
-	}
-	//else use parent miner group
-	//TODO: check after 3 seconds(block creation) and 3 seconds(mining order)
-	index := (block.Header.Time % 9) / 3
-	if minerGroup[index] != block.Header.Coinbase {
-		return errors.New("minerGroup[index] != block.Header.Coinbase")
-	}
-
-	return nil
-
-}
-
-func (ds *DposState) GetMiners(minerHash common.Hash) ([]common.Address, error) {
-	// encodedBytes1, err := rlp.EncodeToBytes(electedTime)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	miner := []common.Address{}
-	decodedBytes, _ := ds.Miner.Get(minerHash[:])
-	rlp.NewStream(bytes.NewReader(decodedBytes), 0).Decode(&miner)
-	return miner, nil
-}
-
-*/
